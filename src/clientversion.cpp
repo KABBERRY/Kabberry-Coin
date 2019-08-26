02c52332872111356f876a29ff7c2290cb523aaa
KrzysztofPrimeStone <36762780+KrzysztofPrimeStone@users.noreply.github.com>
2019-08-26 12:12:26 +0200

    [RPC] mints fix

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -43,10 +43,11 @@ const std::string CLIENT_NAME("PrimeStone");
 #include "obj/build.h"
 #endif
 
-//! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. $Format:%n#define GIT_ARCHIVE 1$
+//! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
+#define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#define GIT_COMMIT_ID "$Format:%H$"
-#define GIT_COMMIT_DATE "$Format:%cD$"
+#define GIT_COMMIT_ID "3c5eef32471cf56484c89a20d5733dbe89d755ef"
+#define GIT_COMMIT_DATE "Mon, 26 Aug 2019 10:59:01 +0200"
 #endif
 
 #define BUILD_DESC_WITH_SUFFIX(maj, min, rev, build, suffix) \
