dbfdbab7d38143e3a83f8901653ddcb45b037c95
Maciej007 <50796483+Maciej007@users.noreply.github.com>
2019-08-31 23:01:33 +0200

    [GUI] Add option to hide orphans in overview and txlist

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -403,6 +403,7 @@ void OverviewPage::setWalletModel(WalletModel* model)
 
         connect(model->getOptionsModel(), SIGNAL(displayUnitChanged(int)), this, SLOT(updateDisplayUnit()));
         connect(model->getOptionsModel(), SIGNAL(hideZeroBalancesChanged(bool)), this, SLOT(updateDisplayUnit()));
+        connect(model->getOptionsModel(), SIGNAL(hideOrphansChanged(bool)), this, SLOT(hideOrphans(bool)));
 
         updateWatchOnlyLabels(model->haveWatchOnly());
         connect(model, SIGNAL(notifyWatchonlyChanged(bool)), this, SLOT(updateWatchOnlyLabels(bool)));
@@ -410,6 +411,10 @@ void OverviewPage::setWalletModel(WalletModel* model)
 
     // update the display unit, to not use the default ("PrimeStone")
     updateDisplayUnit();
+
+      // Hide orphans
+    QSettings settings;
+    hideOrphans(settings.value("fHideOrphans", false).toBool());
 }
 
 void OverviewPage::updateDisplayUnit()
@@ -438,6 +443,12 @@ void OverviewPage::showOutOfSyncWarning(bool fShow)
     ui->labelWalletStatus->setVisible(fShow);
     ui->labelTransactionsStatus->setVisible(fShow);
 }
+
+  void OverviewPage::hideOrphans(bool fHide)
+{
+    filter->setHideOrphans(fHide);
+}
+
 //GET Price from Gecko
 void OverviewPage::syncRequestFinished(QNetworkReply *reply)
 {
