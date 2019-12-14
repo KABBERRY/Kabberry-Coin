400055b409fe8af930a302d98aff3904060cf2a9
KabberryDev <36762780+KabberryDev@users.noreply.github.com>
2019-12-14 13:44:16 +0100

    [UI] Sort numbers correctly in zPSC control dialog

--- src/qt/zPSCcontroldialog.cpp
+++ src/qt/zPSCcontroldialog.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2017-2018 The PIVX developers
-// Copyright (c) 2018-2019 The PrimeStone developers
+// Copyright (c) 2018-2019 The Kabberry developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -43,6 +43,15 @@ void zPSCControlDialog::setModel(WalletModel *model)
     updateList();
 }
 
+// helper function str_pad
+QString zPSCControlDialog::strPad(QString s, int nPadLength, QString sPadding)
+{
+    while (s.length() < nPadLength)
+        s = sPadding + s;
+
+    return s;
+}
+
 //Update the tree widget
 void zPSCControlDialog::updateList()
 {
@@ -61,7 +70,7 @@ void zPSCControlDialog::updateList()
         mapDenomPosition[denom] = ui->treeWidget->indexOfTopLevelItem(itemDenom);
 
         itemDenom->setFlags(flgTristate);
-        itemDenom->setText(COLUMN_DENOMINATION, QString::number(denom));
+        itemDenom->setText(COLUMN_DENOMINATION, strPad(QString::number(denom), 5, " "));
     }
 
     // select all unused coins - including not mature. Update status of coins too.
@@ -85,9 +94,9 @@ void zPSCControlDialog::updateList()
         else
             itemMint->setCheckState(COLUMN_CHECKBOX, Qt::Unchecked);
 
-        itemMint->setText(COLUMN_DENOMINATION, QString::number(mint.denom));
+        itemMint->setText(COLUMN_DENOMINATION, strPad(QString::number(mint.denom), 5, " "));
         itemMint->setText(COLUMN_PUBCOIN, QString::fromStdString(strPubCoinHash));
-        itemMint->setText(COLUMN_VERSION, QString::number(mint.nVersion));
+        itemMint->setText(COLUMN_VERSION, strPad(QString::number(mint.nVersion), 3, " "));
 
         int nConfirmations = (mint.nHeight ? nBestHeight - mint.nHeight : 0);
         if (nConfirmations < 0) {
@@ -95,7 +104,7 @@ void zPSCControlDialog::updateList()
             nConfirmations = 0;
         }
 
-        itemMint->setText(COLUMN_CONFIRMATIONS, QString::number(nConfirmations));
+        itemMint->setText(COLUMN_CONFIRMATIONS, strPad(QString::number(nConfirmations), 15, " "));
 
         // check for maturity
         bool isMature = false;
