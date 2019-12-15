51be9209e2023d54b77e7f2681f343ac59912df9
KabberryDev <36762780+KabberryDev@users.noreply.github.com>
2019-12-15 11:14:18 +0100

    [RPC] include mints metadata in 'listmintedzerocoins' output

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -254,23 +254,13 @@ bool CWallet::LoadMultiSig(const CScript& dest)
 
 bool CWallet::Unlock(const SecureString& strWalletPassphrase, bool anonymizeOnly)
 {
-    SecureString strWalletPassphraseFinal;
-
-    if (!IsLocked()) {
-        fWalletUnlockAnonymizeOnly = anonymizeOnly;
-        return true;
-    }
-
-    strWalletPassphraseFinal = strWalletPassphrase;
-
-
     CCrypter crypter;
     CKeyingMaterial vMasterKey;
 
     {
         LOCK(cs_wallet);
         BOOST_FOREACH (const MasterKeyMap::value_type& pMasterKey, mapMasterKeys) {
-            if (!crypter.SetKeyFromPassphrase(strWalletPassphraseFinal, pMasterKey.second.vchSalt, pMasterKey.second.nDeriveIterations, pMasterKey.second.nDerivationMethod))
+            if (!crypter.SetKeyFromPassphrase(strWalletPassphrase, pMasterKey.second.vchSalt, pMasterKey.second.nDeriveIterations, pMasterKey.second.nDerivationMethod))
                 return false;
             if (!crypter.Decrypt(pMasterKey.second.vchCryptedKey, vMasterKey))
                 continue; // try another master key
@@ -758,6 +748,7 @@ void CWallet::EraseFromWallet(const uint256& hash)
     return;
 }
 
+
 isminetype CWallet::IsMine(const CTxIn& txin) const
 {
     {
@@ -770,27 +761,6 @@ isminetype CWallet::IsMine(const CTxIn& txin) const
         }
     }
     return ISMINE_NO;
-}
-
-bool CWallet::IsUsed(const CBitcoinAddress address) const
-{
-    LOCK(cs_wallet);
-    CScript scriptPubKey = GetScriptForDestination(address.Get());
-    if (!::IsMine(*this, scriptPubKey)) {
-        return false;
-    }
-
-    for (map<uint256, CWalletTx>::const_iterator it = mapWallet.begin(); it != mapWallet.end(); ++it) {
-        const CWalletTx& wtx = (*it).second;
-        if (wtx.IsCoinBase()) {
-            continue;
-        }
-        for (const CTxOut& txout : wtx.vout) {
-            if (txout.scriptPubKey == scriptPubKey)
-                return true;
-        }
-    }
-    return false;
 }
 
 bool CWallet::IsMyZerocoinSpend(const CBigNum& bnSerial) const
