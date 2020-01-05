6cecdeba74476b2733dbc9d72bcc8dc822e0d28d
KabberryDev <36762780+KabberryDev@users.noreply.github.com>
2020-01-05 18:05:09 +0100

    [GUI] Hide orphans

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -403,6 +403,7 @@ void OverviewPage::setWalletModel(WalletModel* model)
 
         connect(model->getOptionsModel(), SIGNAL(displayUnitChanged(int)), this, SLOT(updateDisplayUnit()));
         connect(model->getOptionsModel(), SIGNAL(hideZeroBalancesChanged(bool)), this, SLOT(updateDisplayUnit()));
+        connect(model->getOptionsModel(), SIGNAL(hideOrphansChanged(bool)), this, SLOT(hideOrphans(bool)));
 
         updateWatchOnlyLabels(model->haveWatchOnly());
         connect(model, SIGNAL(notifyWatchonlyChanged(bool)), this, SLOT(updateWatchOnlyLabels(bool)));
@@ -410,6 +411,10 @@ void OverviewPage::setWalletModel(WalletModel* model)
 
     // update the display unit, to not use the default ("Kabberry")
     updateDisplayUnit();
+
+    // Hide orphans
+    QSettings settings;
+    hideOrphans(settings.value("fHideOrphans", false).toBool());
 }
 
 void OverviewPage::updateDisplayUnit()
@@ -478,4 +483,9 @@ void OverviewPage::syncRequestFinished(QNetworkReply *reply)
         ui->labelPricez->setText("NULL");
     }
     delete reply;
+}
+
+void OverviewPage::hideOrphans(bool fHide)
+{
+    filter->setHideOrphans(fHide);
 }
