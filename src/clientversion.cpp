70d9458365487bbf3c3fd9b6a969ade3adf182c0
KabberryDev <36762780+KabberryDev@users.noreply.github.com>
2020-01-05 22:29:50 +0100

    Replace 'boost::lexical_cast<std::string>' with 'std::to_string'

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -43,10 +43,11 @@ const std::string CLIENT_NAME("Kabberry");
 #include "obj/build.h"
 #endif
 
-//! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. $Format:%n#define GIT_ARCHIVE 1$
+//! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
+#define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#define GIT_COMMIT_ID "$Format:%H$"
-#define GIT_COMMIT_DATE "$Format:%cD$"
+#define GIT_COMMIT_ID "d8424e033bb3d3ce03255a52946b90b61314b02c"
+#define GIT_COMMIT_DATE "Sun, 5 Jan 2020 20:51:36 +0100"
 #endif
 
 #define BUILD_DESC_WITH_SUFFIX(maj, min, rev, build, suffix) \
