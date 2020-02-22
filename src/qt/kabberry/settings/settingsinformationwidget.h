d669ac80a5aa4f1bc222966421f9d76b3bd5b027
KabberryDev <36762780+KabberryDev@users.noreply.github.com>
2020-02-22 21:58:26 +0100

    Update settingsinformationwidget.h

--- src/qt/kabberry/settings/settingsinformationwidget.h
+++ src/qt/kabberry/settings/settingsinformationwidget.h
@@ -24,7 +24,7 @@ public:
 
     void loadClientModel() override;
 
-privateQ_SLOTS:
+private Q_SLOTS:
     void setNumConnections(int count);
     void setNumBlocks(int count);
     void openNetworkMonitor();
